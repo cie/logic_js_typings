359732ee43829510a65ed56181023e03d6cd4670
Bernát Kalló <kallo.bernat@gmail.com>
2021-05-23 21:42:58 +0200

    work on deep inference (probably won't work)

--- index.ts
+++ index.ts
@@ -1,6 +1,22 @@
-import { lvar, and, run, eq } from 'logic_js'
+import { and, or, run, eq } from 'logic_js'
 
-const x = lvar<number, 'x'>('x')
-const res = run([x], eq(x, 8))
+const parent = <X extends string | symbol, Y extends string | symbol>(
+  x: X,
+  y: Y,
+) => or(and(eq(x, 'mary'), eq(y, 'john')), and(eq(x, 'mary'), eq(y, 'john')))
 
-type A = [number]
+const x = Symbol()
+const y = Symbol()
+const res = run([x, y], parent(x, y))
+
+type A = { [k in number] }
+
+let l = () => {
+  const a = Symbol(),
+    b = Symbol()
+  return [a, b] as [typeof a, typeof b]
+}
+const d = l()
+const h = l()
+if (d[0] == h[0]) {
+}
--- logic_js.d.ts
+++ logic_js.d.ts
@@ -1,32 +1,46 @@
 declare module 'logic_js' {
   const TYPE: unique symbol
-  interface LVar<Type, ID extends string | undefined> {
-    [TYPE]: Type
-    constructor: { name: 'LVar' }
-    id: ID
-  }
+  const MAPPINGS: unique symbol
   interface ImmutableMap {
     constructor: { name: 'ImmutableMap' }
   }
-  type Clause = (
-    sMap: ImmutableMap,
-  ) => Generator<ImmutableMap | null, void, never>
-  type Lookup<A, B extends LVar<any, any>[]> = B extends [infer H, ...(infer T)]
-    ? H
+  type Clause<M> = {
+    [MAPPINGS]: M
+    (sMap: ImmutableMap): Generator<ImmutableMap | null, void, never>
+  }
+  type ClauseFn<M> = () => Clause<M>
+  type ClauseOrFn<M> = Clause<M> | ClauseFn<M>
+  type Mapping<CF> = CF extends Clause<infer M>
+    ? M
+    : CF extends ClauseFn<infer M>
+    ? M
     : never
-  type Resolve<V> = V extends LVar<infer T, any> ? V | T : never
+  type Mappings<CFS> = { [K in keyof CFS]: Mapping<CFS[K]> }
+  type And<MS extends {}[]> = MS extends [infer H, ...(infer T)]
+    ? H & And<T>
+    : {}
+  type Or<MS extends {}[]> = MS extends [infer H, ...(infer T)]
+    ? H | Or<T>
+    : never
   const x: {
-    lvar<Type>(): LVar<Type, string>
-    lvar<Type, ID extends string>(id: ID): LVar<Type, ID>
-    eq<X>(a: X | LVar<X, any>, b: X | LVar<X, any>): Clause
-    and(...clauses: (Clause | (() => Clause))[]): Clause
-    or(count: number, ...clauses: (Clause | (() => Clause))[]): Clause
-    or(...clauses: (Clause | (() => Clause))[]): Clause
-    run<Vars extends LVar<any, any>[]>(
+    eq<A extends symbol, B>(a: A, b: B): Clause<{ [a in A]: B }>
+    eq<A, B extends symbol>(a: A, b: B): Clause<{ [b in B]: A }>
+    eq<A>(a: A, b: A): Clause<{}>
+    and<CS extends ClauseOrFn<any>[]>(
+      ...clauses: [...CS]
+    ): Clause<And<Mappings<CS>>>
+    or<CS extends ClauseOrFn<any>[]>(
+      count: number,
+      ...clauses: [...CS]
+    ): Clause<Or<Mappings<CS>>>
+    or<CS extends ClauseOrFn<any>[]>(
+      ...clauses: [...CS]
+    ): Clause<Or<Mappings<CS>>>
+    run<Vars extends symbol[], M extends { [k in Vars[number]] }>(
       vars: [...Vars],
-      goal: Clause | (() => Clause),
-    ): { [Var in Vars[number]['id']]: Resolve<Lookup<Var, Vars>> }[]
-    facts<Args extends any[]>(...facs: Args[]): (...args: Args) => Clause
+      goal: ClauseOrFn<M>,
+    ): { [Var in Vars[number]]: M[Var] }[]
+    //facts<Args extends any[]>(...facs: Args[]): (...args: Args) => Clause
   }
   export = x
 }
