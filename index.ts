736017035c5509fa0e26a1eabf39dff00ad9b99f
Bernát Kalló <kallo.bernat@gmail.com>
2021-05-23 23:04:49 +0200

    add all functions

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
-import { lvar, and, or, run, eq } from 'logic_js'
+import { facts, lvar, and, or, run, eq, conso } from 'logic_js'
 
-const parent = (x: number, y: string) =>
-  or(
-    and(eq(x, 4), eq(y, 'john')),
-    and(eq(x, 4), eq(y, 'john')),
-    and(eq(x, 4), eq(y, 'jeff')),
-  )
+const parent = facts(['Mary', 'John'], ['Mary', 'Joe'], ['Joe', 'Jim'])
+const ancestor = (a: string, b: string) =>
+  or(parent(a, b), (x = lvar<string>()) => and(parent(a, x), ancestor(x, b)))
 
-const x = lvar<number, 'x'>('x')
+const x = lvar<string, 'x'>('x')
 const y = lvar<string, 'y'>('y')
-const res = run([x, y], parent(x, y))
+console.log(run([x, y], ancestor(x, y)))
+
+const k = lvar<string[], 'k'>('k')
+console.log(run([x, k], conso(x, k, ['a', 'b', 'c'])))
--- logic_js.d.ts
+++ logic_js.d.ts
@@ -1,6 +1,6 @@
 declare module 'logic_js' {
+  const VAR: unique symbol
   type LVar<T, name extends string> = T & { [VAR]: name }
-  const TYPE: unique symbol
   interface ImmutableMap {
     constructor: { name: 'ImmutableMap' }
   }
@@ -16,20 +16,44 @@ declare module 'logic_js' {
 
   type ClauseFn = () => Clause
   type ClauseOrFn = Clause | ClauseFn
-  const VAR: unique symbol
 
   const x: {
     lvar<T>(): LVar<T, `~.${any}`>
     lvar<T, name extends string>(name: name): LVar<T, name>
-    eq<A>(a: A, b: A): Clause
+    run<Vars extends LVar<any, any>[]>(
+      count: number,
+      vars: [...Vars],
+      goal: ClauseOrFn,
+    ): { [name in Vars[number][typeof VAR]]: Lookup<name, Vars> }[]
+    run<Vars extends LVar<any, any>[]>(
+      vars: [...Vars],
+      goal: ClauseOrFn,
+    ): { [name in Vars[number][typeof VAR]]: Lookup<name, Vars> }[]
     and(...clauses: ClauseOrFn[]): Clause
     or(count: number, ...clauses: ClauseOrFn[]): Clause
     or(...clauses: ClauseOrFn[]): Clause
-    run<Vars extends LVar<any, any>[]>(
-      vars: [...Vars],
-      goal: Clause | (() => Clause),
-    ): { [name in Vars[number][typeof VAR]]: Lookup<name, Vars> }[]
-    facts<Args extends any[]>(...facs: Args[]): (...args: Args) => Clause
+    eq<A>(a: A, b: A): Clause
+    facts<Args extends any[]>(...facs: [...Args][]): (...args: Args) => Clause
+    conso<A, B extends any[]>(a: A, b: B, l: [A, ...B]): Clause
+    firsto<A>(a: A, l: [A, ...any]): Clause
+    resto<L extends any[]>(a: L, l: [any, ...L]): Clause
+    emptyo(l: any[]): Clause
+    membero<A>(a: A, l: A[]): Clause
+    appendo<A>(a: A[], b: A[], l: A[]): Clause
+    add(a: number, b: number, c: number): Clause
+    sub(a: number, b: number, c: number): Clause
+    mul(a: number, b: number, c: number): Clause
+    div(a: number, b: number, c: number): Clause
+    lt(a: number, b: number): Clause
+    le(a: number, b: number): Clause
+    gt(a: number, b: number): Clause
+    ge(a: number, b: number): Clause
+    stringo(a: any): Clause
+    numbero(a: any): Clause
+    arrayo(a: any): Clause
+    succeed(): Clause
+    fail(): Clause
+    anyo(goal: ClauseOrFn): Clause
   }
   export = x
 }
