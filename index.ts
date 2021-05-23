d6dc662c59fa011f6af81c6e4ac8388652d434ea
Bernát Kalló <kallo.bernat@gmail.com>
2021-05-23 22:26:10 +0200

    lvar is not specific

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
-import { lvar, and, run, eq } from 'logic_js'
+import { lvar, and, or, run, eq } from 'logic_js'
+
+const parent = (x: number, y: string) =>
+  or(
+    and(eq(x, 4), eq(y, 'john')),
+    and(eq(x, 4), eq(y, 'john')),
+    and(eq(x, 4), eq(y, 'jeff')),
+  )
 
 const x = lvar<number, 'x'>('x')
-const res = run([x], eq(x, 8))
-
-type A = [number]
+const y = lvar<string, 'y'>('y')
+const res = run([x, y], parent(x, y))
